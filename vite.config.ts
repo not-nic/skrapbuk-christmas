52cda33b67c97b353a6ac41519ed6e9972f03eaf
notnic <nicovo989@gmail.com>
2023-10-03 01:44:00 +0100

    Refactor: CORS removal, logo SVG, CSS tweaks.

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    cors: true,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8080",
+        target: "http://127.0.0.1:8080/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
