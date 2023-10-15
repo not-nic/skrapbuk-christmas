9efe1e0d495955948583f6ee79bdec7fb02a505c
notnic <nicovo989@gmail.com>
2023-10-15 18:58:28 +0100

    Create ban_list database model.

---
+++ python/classes/ban_list.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from python.classes.database import database
+
+class BanList(database.Model):
+    id = Column(Integer, primary_key=True)
+    user_snowflake = Column(String(255), ForeignKey('user.snowflake'))
+    reason = Column(Text)
+    banned_user = database.relationship('User')
+
+    def __int__(self, user_snowflake, reason, banned_user):
+        self.user_snowflake = user_snowflake
+        self.reason = reason
+        self.banned_user = banned_user
