77eeb0e2bb7dd7c65e4a7314e3b2b2ac67deed78
notnic <nicovo989@gmail.com>
2023-10-23 01:01:59 +0100

    Create has_partner decorator function.

--- python/config.py
+++ python/config.py
@@ -4,6 +4,7 @@ import time
 from functools import wraps
 from flask_discord import DiscordOAuth2Session
 from flask import abort, request
+from python.classes.models.user import User
 from python.classes.models.ban_list import BanList
 
 class Config:
@@ -69,7 +70,7 @@ class Config:
     def is_admin(self, func):
         """
         Decorator function to check if a user is an admin.
-        Returns
+        Returns:
             (nothing) continues with the function if the user is an admin.
             (abort) 401: Unauthorised message if the user is not an admin.
         """
@@ -91,9 +92,9 @@ class Config:
     def is_banned(self, func):
         """
         Decorator function to check if a user is banned from Skrapbuk.
-        Returns
+        Returns:
             (nothing) continues with the function if the user not banned.
-            (abort) 401: Unauthorised message if the user is banned.
+            (abort) 403: Forbidden message if the user is banned.
         """
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -109,6 +110,31 @@ class Config:
                     message_type="INFO"
                 )
 
-                return abort(code=401, description=f"You are banned, you cannot take part in Skrapbuk.")
+                return abort(code=403, description=f"You are banned, you cannot take part in Skrapbuk.")
+
+        return wrapper
+
+    def has_partner(self, func):
+        """
+        Decorator Function to check if a user has been assigned a partner.
+        Returns:
+            (nothing) continues with the function if the user has a partner.
+            (abort) 403: User is forbidden from viewing content.
+        """
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            discord = DiscordOAuth2Session()
+            snowflake = discord.fetch_user().id
+            user = User.query.filter_by(snowflake=snowflake).first()
+
+            # check if the user exists and has a partner
+            if user and user.partner:
+                 return func(*args, **kwargs)
+            else:
+                self.logger.queue_message(
+                    message=f"{discord.fetch_user().username} ({discord.fetch_user().id}) does not have a partner",
+                    message_type="INFO"
+                )
+                return abort(code=403, description=f"User ({snowflake}) does not have a partner.")
 
         return wrapper
