31589244f10fff4258aeede66d79cafd0ffbbe72
notnic <nicovo989@gmail.com>
2023-11-01 01:37:50 +0000

    Remove: created_at from artwork.py constructor.

--- python/classes/models/artwork.py
+++ python/classes/models/artwork.py
@@ -8,7 +8,6 @@ class Artwork(database.Model):
     image_path = Column(String(255))
     created_at = Column(TIMESTAMP, server_default=func.now())
 
-    def __init__(self, created_by, image_path, created_at):
+    def __init__(self, created_by, image_path):
         self.created_by = created_by,
         self.image_path = image_path,
-        self.created_at = created_at
