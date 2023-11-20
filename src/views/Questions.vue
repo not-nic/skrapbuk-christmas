687ccc58e8fafab66be011a4d33914796023c5de
notnic <nicovo989@gmail.com>
2023-11-20 01:36:29 +0000

    (WIP) Refactor frontend structure, created temporary components & views.

---
+++ src/views/Questions.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "Questions"
+})
+</script>
+
+<template>
+  <div class="questions">
+    <h1>Answer questions here!</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
