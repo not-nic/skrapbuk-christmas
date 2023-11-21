533f509130bb238d5743ae77140dae8918801d0b
notnic <nicovo989@gmail.com>
2023-11-21 23:04:16 +0000

    (WIP) Added 404 page for invalid endpoints.

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,14 @@ import Home from "../views/Home.vue";
 import Profile from "../views/Profile.vue";
 import Signup from "../views/Signup.vue";
 import Questions from "../views/Questions.vue";
+import PageNotFound from "../views/PageNotFound.vue";
 
 const routes = [
     { path: "/", name: "home", component: Home },
     { path: "/profile", name: "profile", component: Profile },
     { path: "/signup", name: "signup", component: Signup },
-    { path: "/questions", name: "questions", component: Questions }
+    { path: "/questions", name: "questions", component: Questions },
+    { path: "/:pathMatch(.*)*", name: 'notFound', component: PageNotFound }
 ]
 
 const router = createRouter({
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import Grid from "../components/Grid.vue";
+
+export default defineComponent({
+  name: "PageNotFound",
+  components: {Grid}
+})
+</script>
+
+<template>
+  <div class="not-found">
+    <Grid></Grid>
+    <h1>404!</h1>
+    <h3>Gom tried his hardest to find that page, but he couldn't! :(</h3>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
