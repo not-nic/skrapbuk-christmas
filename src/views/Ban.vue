7e53fdc8282c009812e63a1d121cb617a9141735
notnic <nicovo989@gmail.com>
2023-11-23 01:15:09 +0000

    Created Join & Ban vue components.

---
+++ src/views/Ban.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "Ban"
+})
+</script>
+
+<template>
+  <div>
+    <h1>
+      you're banned lad
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
