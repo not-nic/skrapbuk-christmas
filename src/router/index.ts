a36533848ba0853aa627165b22d4e710ba745af9
notnic <nicovo989@gmail.com>
2023-11-23 01:19:27 +0000

    Added additional routes to router.

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,16 @@ import Profile from "../views/Profile.vue";
 import Signup from "../views/Signup.vue";
 import Questions from "../views/Questions.vue";
 import PageNotFound from "../views/PageNotFound.vue";
+import Join from "../views/Join.vue";
+import Ban from "../views/Ban.vue";
 
 const routes = [
     { path: "/", name: "home", component: Home },
     { path: "/profile", name: "profile", component: Profile },
     { path: "/signup", name: "signup", component: Signup },
     { path: "/questions", name: "questions", component: Questions },
+    { path: "/join", name: "join", component: Join },
+    { path: "/banned", name: "banned", component: Ban },
     { path: "/:pathMatch(.*)*", name: 'notFound', component: PageNotFound }
 ]
 
