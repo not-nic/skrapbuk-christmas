ff35a3c753442844802e35618f07da30280b7e0d
notnic <nicovo989@gmail.com>
2023-11-26 02:35:08 +0000

    added custom error handler for generic responses, i.e. redirect & banned.

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,26 +1,21 @@
 import { defineStore } from "pinia";
 import axios from "axios";
-
-interface Profile {
-    username: string,
-    snowflake: number,
-    avatar_url: string,
-    in_server: boolean,
-    is_admin: boolean,
-    is_banned: boolean,
-    partner: number
-}
+import {Partner} from "../ts/Partner.ts";
+import {Profile} from "../ts/Profile.ts";
 
 export const useUserStore = defineStore('userStore', {
     state: () => ({
         user: {} as Profile,
+        partner: {} as Partner,
+
         rotateDeg: 0,
         defaultImg: "../src/assets/gom.webp",
         clickCount: 0,
         showCopy: false,
 
         revealedCard: true,
-        eventStarted: true
+        eventStarted: true,
+        showNoPartnerMessage: false,
     }),
 
     actions: {
@@ -38,6 +33,41 @@ export const useUserStore = defineStore('userStore', {
         },
 
         /**
+         * Custom error handler for backend responses, uses a switch case on known responses such as 401 or 403.
+         * @param error - Axios error object
+         * @param customHandlers - Custom response such as an error message contents, or boolean to activate a v-if.
+         */
+        async errorHandler(error: any,
+                           customHandlers: { customErrorHandler?: (error: any) => void} = {}): Promise<void> {
+            // get status code from response
+            let httpStatus = error.response?.status;
+
+            const handleDefault = () => {
+                // TODO: Redirect user to an error page, on requests statuses like 500.
+                console.error('Unhandled error:', error);
+            };
+
+            switch(httpStatus) {
+                // if the user is unauthorised, redirect them to the login page.
+                case 401:
+                    window.location.href = "/api"
+                    break;
+                // 403 is returned from banned users, redirect them to /banned.
+                case 403:
+                    this.$router.push("banned");
+                    break;
+                // 400 errors can require different edge cases such, i.e. an error message.
+                case 400:
+                    if (customHandlers.customErrorHandler) {
+                        customHandlers.customErrorHandler(error)
+                    }
+                    break;
+                // if no errors are found, log the unhandled error.
+                default: handleDefault();
+            }
+        },
+
+        /**
          * Asynchronous function to get an authorised users discord details from Flask.
          */
         async getUser() {
@@ -45,20 +75,26 @@ export const useUserStore = defineStore('userStore', {
                 // if authed, get the user's details and add them to the user object.
                 const response = await axios.get("/api/users/me", {withCredentials: true})
                 this.user = response.data;
-            } catch (error: any) {
-
-                let http_status = error.response?.status;
-
-                // check if the user is unauthorised, if so redirect them to the login page.
-                if (http_status === 401) {
-                    window.location.href = "/api";
-                } else if (http_status === 403) {
-                    this.$router.push("banned")
-                } else {
-                    console.error('Non-401/403 error:', error);
-                }
+            } catch (error) {
+                await this.errorHandler(error)
             }
         },
+
+        /**
+         * Asynchronous function to get an authorised users partners from Flask.
+         */
+        async getPartner() {
+            try {
+                const response = await axios.get("/api/users/partner", {withCredentials: true})
+                this.partner = response.data
+            } catch (error: any) {
+                await this.errorHandler(error, {
+                    customErrorHandler: () => {
+                        this.showNoPartnerMessage = true
+                    }
+                })
+            }
+         },
 
         /**
          * Allow a user to copy their own discord snowflake to clipboard.
@@ -77,7 +113,7 @@ export const useUserStore = defineStore('userStore', {
          * Toggle to see if the user has revealed their partner, by saving a key/value to local storage.
          */
         revealPartner(): void {
-            const hasRevealed = localStorage.getItem('revealedCard') === 'true';
+            const hasRevealed: boolean = localStorage.getItem('revealedCard') === 'true';
             if (!hasRevealed) {
                 localStorage.setItem('revealedCard', 'true');
                 this.revealedCard = true;
